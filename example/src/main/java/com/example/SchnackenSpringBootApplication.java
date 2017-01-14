9d5d62a7f5ccaab7b2b5b7f066dfd65058f9b707
Olle Törnström <olle@studiomediatech.com>
2017-01-14 15:22:54 +0100

    Add example skeleton project

---
+++ example/src/main/java/com/example/SchnackenSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchnackenSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchnackenSpringBootApplication.class, args);
+	}
+}
